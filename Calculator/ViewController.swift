6afedb15f4daf763dee425610eaf90f14769851a
krlargo <kevinlargo@live.com>
2016-09-08 02:58:17 -0700

    Connected UIButtons to View Controller. Still missing functionality for positive/negative button, percentage button, and decimal button. Otherwise, calculator is fully functional for integers expressions using addition, subtraction, multiplication, and division operators.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Operation {
+  case none
+  case division
+  case multiplication
+  case subtraction
+  case addition
+}
 
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var displayLabel: UILabel!
+  var totalValue = CGFloat(0); //holds entire equation
+  var displayValue = CGFloat(0); //holds values for current terms
+
+  @IBOutlet weak var allClearButton: UIButton!
+  @IBOutlet weak var positiveNegativeButton: UIButton!
+  @IBOutlet weak var percentageButton: UIButton!
+  
+  @IBOutlet var numberButtons: [UIButton]!
+  @IBOutlet weak var decimalButton: UIButton!
+  
+  @IBOutlet weak var divideButton: UIButton!
+  @IBOutlet weak var multiplyButton: UIButton!
+  @IBOutlet weak var subtractButton: UIButton!
+  @IBOutlet weak var addButton: UIButton!
+  @IBOutlet weak var equalsButton: UIButton!
+  
+  var currentOperation = Operation.none;
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +46,128 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  func updateDisplay() {
+    displayLabel.text = String(displayValue);
+  }
+  
+  @IBAction func zeroPressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 0;
+    updateDisplay();
+  }
+  
+  @IBAction func onePressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 1;
+    updateDisplay();
+  }
+  
+  @IBAction func twoPressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 2;
+    updateDisplay();
+  }
+  
+  @IBAction func threePressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 3;
+    updateDisplay();
+  }
+  
+  @IBAction func fourPressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 4;
+    updateDisplay();
+  }
+  
+  @IBAction func fivePressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 5;
+    updateDisplay();
+  }
+  
+  @IBAction func sixPressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 6;
+    updateDisplay();
+  }
+  
+  @IBAction func sevenPressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 7;
+    updateDisplay();
+  }
+  
+  @IBAction func eightPressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 8;
+    updateDisplay();
+  }
+  
+  @IBAction func ninePressed(sender: AnyObject) {
+    displayValue *= 10;
+    displayValue += 9;
+    updateDisplay();
+  }
+  
 
-
+  @IBAction func allClearPressed(sender: AnyObject) {
+    displayValue = 0;
+    currentOperation = Operation.none;
+    updateDisplay();
+  }
+  
+  
+  @IBAction func divisionPressed(sender: AnyObject) {
+    currentOperation = Operation.division;
+    totalValue = displayValue;
+    displayValue = 0;
+  }
+  
+  @IBAction func multiplicationPressed(sender: AnyObject) {
+    currentOperation = Operation.multiplication;
+    totalValue = displayValue;
+    displayValue = 0;
+  }
+  
+  @IBAction func subtractionPressed(sender: AnyObject) {
+    currentOperation = Operation.subtraction;
+    totalValue = displayValue;
+    displayValue = 0;
+  }
+  
+  @IBAction func additionPressed(sender: AnyObject) {
+    currentOperation = Operation.addition;
+    totalValue = displayValue;
+    displayValue = 0;
+  }
+  
+  @IBAction func evaluatePressed(sender: AnyObject) {
+    switch (currentOperation) {
+    case Operation.division:
+      totalValue /= displayValue;
+      break;
+      
+    case Operation.multiplication:
+      totalValue *= displayValue;
+      break;
+      
+    case Operation.subtraction:
+      totalValue -= displayValue;
+      break;
+      
+    case Operation.addition:
+      totalValue += displayValue;
+      break;
+      
+    default:
+      return;
+    }
+    
+    displayValue = totalValue;
+    updateDisplay();
+    
+  }
+  
 }
-
