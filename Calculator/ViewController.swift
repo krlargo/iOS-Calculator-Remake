43433969afe102eb258b80e95a34a61b19d8590d
krlargo <kevinlargo@live.com>
2016-09-08 11:51:09 -0700

    Decimal input and arithmetic is fully functioning; made minor text changes to optimize code reusability.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -19,8 +19,16 @@ enum Operation {
 class ViewController: UIViewController {
   
   @IBOutlet weak var displayLabel: UILabel!
+  
   var totalValue = CGFloat(0); //holds entire equation
-  var displayValue = CGFloat(0); //holds values for current terms
+  var displayValue = CGFloat(0); //value currently being displayed
+  var secondTermValue = CGFloat(0); //holds values for current terms
+  var currentOperation = Operation.none;
+  var decimalPressed = false;
+  var decimalPlace = CGFloat(1);
+  var evaluated = false; //if current display is evaluated
+  var beginNewTerm = false;
+
 
   @IBOutlet weak var allClearButton: UIButton!
   @IBOutlet weak var positiveNegativeButton: UIButton!
@@ -35,7 +43,6 @@ class ViewController: UIViewController {
   @IBOutlet weak var addButton: UIButton!
   @IBOutlet weak var equalsButton: UIButton!
   
-  var currentOperation = Operation.none;
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -48,117 +55,162 @@ class ViewController: UIViewController {
   }
   
   func updateDisplay() {
-    displayLabel.text = String(displayValue);
+    if(decimalPressed) { //display value as is
+      displayLabel.text = String(displayValue);
+    } else { //display value as integer (no decimal)
+      displayLabel.text = String(Int(displayValue));
+    }
+  }
+  
+  func shiftDecimalPlace() {
+    decimalPlace /= 10;
+  }
+  
+  func processDigit(x: CGFloat) {
+    if(displayValue * 10 > 999999999) { //if next inputted integer breaks max
+      return; //don't process anymore digits
+    }
+    
+    //if inputting term after operation button was tapped
+    if(beginNewTerm) {
+      displayValue = 0;
+      beginNewTerm = false;
+    }
+    
+    if(decimalPressed) {
+      shiftDecimalPlace();
+      displayValue += x * decimalPlace;
+      
+    } else {
+      displayValue *= 10;
+      displayValue += x;
+    }
+    
+    secondTermValue = displayValue;
+    
+    updateDisplay();
   }
   
   @IBAction func zeroPressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 0;
-    updateDisplay();
+    processDigit(0);
   }
   
   @IBAction func onePressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 1;
-    updateDisplay();
-  }
+    processDigit(1);
+ }
   
   @IBAction func twoPressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 2;
-    updateDisplay();
-  }
+    processDigit(2);
+ }
   
   @IBAction func threePressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 3;
-    updateDisplay();
+    processDigit(3);
   }
   
   @IBAction func fourPressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 4;
-    updateDisplay();
+    processDigit(4);
   }
   
   @IBAction func fivePressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 5;
-    updateDisplay();
+    processDigit(5);
   }
   
   @IBAction func sixPressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 6;
-    updateDisplay();
+    processDigit(6);
   }
   
   @IBAction func sevenPressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 7;
-    updateDisplay();
+    processDigit(7);
   }
   
   @IBAction func eightPressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 8;
-    updateDisplay();
+    processDigit(8);
   }
   
   @IBAction func ninePressed(sender: AnyObject) {
-    displayValue *= 10;
-    displayValue += 9;
-    updateDisplay();
+    processDigit(9);
   }
   
+  
+  @IBAction func decimalPressed(sender: AnyObject) {
+    //decimal button can only be pressed once per term
+    if(decimalPressed) {
+      return;
+    }
+    
+    if(beginNewTerm) {
+      displayValue = 0;
+      updateDisplay();
+      beginNewTerm = false;
+    }
+    
+    decimalPressed = true;
+    displayLabel.text! += ".";
+  }
 
   @IBAction func allClearPressed(sender: AnyObject) {
-    displayValue = 0;
     currentOperation = Operation.none;
+    displayValue = 0;
+    secondTermValue = 0;
+    decimalPressed = false;
+    decimalPlace = 1;
     updateDisplay();
   }
   
+  func resetForNewTerm() {
+    displayValue = 0; //value itself is reset, not on screen label text
+//    secondTermValue = 0;
+    decimalPressed = false;
+    decimalPlace = 1;
+    beginNewTerm = true;
+  }
+  
+  var lastPressed = Operation.none;
+  
+  func setOperation(op: Operation) {
+    
+    currentOperation = op;
+    totalValue = displayValue; //whatever is displayed is new total
+
+    resetForNewTerm(); //reset for next input
+  }
   
   @IBAction func divisionPressed(sender: AnyObject) {
-    currentOperation = Operation.division;
-    totalValue = displayValue;
-    displayValue = 0;
+    setOperation(Operation.division);
   }
   
   @IBAction func multiplicationPressed(sender: AnyObject) {
-    currentOperation = Operation.multiplication;
-    totalValue = displayValue;
-    displayValue = 0;
+    setOperation(Operation.multiplication);
   }
   
   @IBAction func subtractionPressed(sender: AnyObject) {
-    currentOperation = Operation.subtraction;
-    totalValue = displayValue;
-    displayValue = 0;
+    setOperation(Operation.subtraction);
   }
   
   @IBAction func additionPressed(sender: AnyObject) {
-    currentOperation = Operation.addition;
-    totalValue = displayValue;
-    displayValue = 0;
+    setOperation(Operation.addition);
   }
   
   @IBAction func evaluatePressed(sender: AnyObject) {
+    evaluate();
+  }
+  
+  func evaluate() {
     switch (currentOperation) {
     case Operation.division:
-      totalValue /= displayValue;
+      totalValue /= secondTermValue;
       break;
       
     case Operation.multiplication:
-      totalValue *= displayValue;
+      totalValue *= secondTermValue;
       break;
       
     case Operation.subtraction:
-      totalValue -= displayValue;
+      totalValue -= secondTermValue;
       break;
       
     case Operation.addition:
-      totalValue += displayValue;
+      totalValue += secondTermValue;
       break;
       
     default:
@@ -168,6 +220,7 @@ class ViewController: UIViewController {
     displayValue = totalValue;
     updateDisplay();
     
+    currentOperation = Operation.none;
+    evaluated = true;
   }
-  
 }
